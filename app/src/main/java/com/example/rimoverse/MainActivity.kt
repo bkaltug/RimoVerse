2e5a11c2175e4de8cfdb1b281fcae11d29d1f140
berkay.altug <berkay.ustagul@gmail.com>
2023-08-21 17:08:52 +0300

    break point

--- app/src/main/java/com/example/rimoverse/MainActivity.kt
+++ app/src/main/java/com/example/rimoverse/MainActivity.kt
@@ -2,44 +2,16 @@ package com.example.rimoverse
 
 import android.app.Fragment
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.rimoverse.ui.theme.RimoVerseTheme
+import androidx.fragment.app.FragmentTransaction
+import com.example.rimoverse.fragments.SplashFragment
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        val button1: Button = findViewById(R.id.button1)
-        val button2: Button = findViewById(R.id.button2)
-        val button3: Button = findViewById(R.id.button3)
-
-        button1.setOnClickListener {
-            changeFragment(FragmentOne())
-        }
-
-        button2.setOnClickListener {
-            changeFragment(FragmentTwo())
-        }
-
-        button3.setOnClickListener {
-            changeFragment(FragmentThree())
-        }
     }
 
-    fun changeFragment(fragment: Fragment){
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
-        fragmentTransaction.commit()
-    }
+
 }
---
+++ app/src/main/java/com/example/rimoverse/fragments/SplashFragment.kt
@@ -0,0 +1,35 @@
+package com.example.rimoverse.fragments
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.rimoverse.R
+
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_splash, container, false)
+
+
+
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_characterListFragment)
+        },3000)
+
+        return view
+    }
+}
